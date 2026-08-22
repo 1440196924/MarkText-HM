bc3f8ecc6204ec1d9d5d56550436094f775c2cb6
sdddswsf <1440196924@qq.com>
2026-08-23 01:11:35 +0800

    chore: untrack build artifacts and cache files, refine .gitignore

--- scripts/port/app/web/assets/channel-ptEtP17z.js
+++
@@ -1,7 +0,0 @@
-import { U as Utils, C as Color } from "./mermaid.core-ChV6Ok9d.js";
-const channel = (color, channel2) => {
-  return Utils.lang.round(Color.parse(color)[channel2]);
-};
-export {
-  channel as c
-};
